b112d9d4060f794a0f694391cd1cd06aca7dee8b
Bernardo Viseu <bernardo2viseu@gmail.com>
2021-06-17 17:21:04 +0100

    requirements updated and scripts deleted

--- scripts/list_all.py
+++
@@ -1,9 +0,0 @@
-from py2neo import Graph, NodeMatcher
-g = Graph("bolt://localhost:7687",password='cartos', user='neo4j') 
-result = g.run('Match (x:Elemento) return x')
-
-for i in result:
-    print(i[0]['id'])
-    result2 = g.run('match (e:Elemento)-[]->(c:Colecao) where e.id="ELEM-5" return c.designacao')
-    for j in result2:
-        print("j = {j[0]['id']}")
